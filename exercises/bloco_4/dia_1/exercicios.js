06dfc710bae2e7e99aa620ac31a96326d8cc12ea
Leonidas Junior <leonautcco@gmail.com>
2021-06-23 19:40:25 -0300

    Inicio dos exercios

---
+++ exercises/bloco_4/dia_1/exercicios.js
@@ -0,0 +1,79 @@
+// EXERCICIO 1
+// let a = 8;
+// let b = 2;
+
+// console.log(`O resultado da soma é: ${a + b}`);
+// console.log(`O resultado da subtração é: ${a - b}`);
+// console.log(`O resultada da multiplicação é: ${8 * 2}`);
+// console.log(`O resultado da divisão é: ${8 / 2}`);
+// console.log(`O resultado do módulo é: ${8 % 2}`);
+// FIM EXERCICIO 1
+
+// EXERCICIO 2
+// let a = 8;
+// let b = 2;
+// if (a > b) {
+//   console.log(`O número maior é: A`);
+// } else {
+//   console.log(`O maior é: B`);
+// }
+// FIM EXERCICIO 2
+
+// EXERCICIO 3
+// let a = 28;
+// let b = 200;
+// let c = 15;
+
+// if (a > b && a > c) {
+//   console.log(`O número maior é A`);
+// }
+// else if (b > c) {
+//   console.log(`O número maior é B`);
+// }
+// else {
+//   console.log(`O número maior é C`);
+// }
+// FIM DO EXERCICIO 3
+
+// EXERCICIO 4
+// let valor = 0;
+
+// if (valor > 0) {
+//   console.log(`Número positivo`);
+// }
+// else if (valor < 0) {
+//   console.log(`Número negativo`);
+// } else {
+//   console.log(`O número é zero`);
+// }
+//FIM DO EXERCICIO 4
+
+//  EXERCICIO 5
+// let angulo1 = 100;
+// let angulo2 = 30;
+// let angulo3 = 50;
+// let angulosTriangulo = angulo1 + angulo2 + angulo3;
+// let angulosPositivos = angulo1 > 0 && angulo2 > 0 && angulo3 > 0;
+
+// if (angulosTriangulo === 180) {
+// console.log(`true`);
+// } else {
+// console.log(`false`);
+// };
+// if (angulosPositivos){
+// } else {
+//   console.log(`O ângulo não é válido`);
+// }
+// FIM DO EXERCICIO 5
+
+// EXERCICIO 6
+let pecaXadrez = 'peao';
+
+switch (pecaXadrez) {
+  case value:
+    
+    break;
+
+  default:
+    break;
+}
