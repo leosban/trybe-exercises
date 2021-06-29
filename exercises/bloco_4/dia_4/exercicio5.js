5ac0c11a87983434eb82428ed9acd4aa3cb0c8d6
Leonidas Junior <leonautcco@gmail.com>
2021-06-28 22:08:55 -0300

    Solucao exercicio5.js

--- exercises/bloco_4/dia_4/exercicio5.js
+++ exercises/bloco_4/dia_4/exercicio5.js
@@ -5,3 +5,17 @@ let info = {
   recorrente: 'Sim'
 };
 
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim'
+}
+
+for (const key in info, info2) {
+  if (info[key] === info2[key]) {
+      console.log(`Ambos recorrentes`);
+  } else {
+    console.log(`${info[key]} e ${info2[key]}`);
+  }
+}
