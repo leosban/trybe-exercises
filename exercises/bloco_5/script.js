8fdfed59c7b6f2467ffa3a0b022817982ecb875b
Leonidas Junior <leonautcco@gmail.com>
2021-06-30 19:29:30 -0300

    Solucao exercicios fixacao

---
+++ exercises/bloco_5/script.js
@@ -0,0 +1,15 @@
+document.getElementById('header-container').style.backgroundColor = 'green';
+
+document.querySelector('.emergency-tasks').style.backgroundColor = 'red';
+
+document.querySelector('.no-emergency-tasks').style.backgroundColor = 'yellow'
+
+document.querySelector('.emergency-tasks .sub1').style.backgroundColor = 'blue';
+
+document.querySelector('.emergency-tasks .sub2').style.backgroundColor = 'blue';
+
+document.querySelector('.no-emergency-tasks .sub1').style.backgroundColor = 'green';
+
+document.querySelector('.no-emergency-tasks .sub2').style.backgroundColor = 'green';
+
+document.getElementById('footer-container').style.backgroundColor = 'black'
