d758dfcd2093b471caa509bb14cf475165cf0ccc
Leonidas Junior <leonautcco@gmail.com>
2021-07-05 15:17:32 -0300

    Solucao fixacao 1

---
+++ exercises/bloco_5/dia_2/fixacao1.js
@@ -0,0 +1,30 @@
+document.querySelector('#elementoOndeVoceEsta');
+
+document.querySelector('#elementoOndeVoceEsta').parentNode.style.color = "red";
+
+document.querySelector('#primeiroFilhoDoFilho').innerText = "Esse é o primeiro Filho do Filho!";
+
+document.querySelector('#pai').firstElementChild;
+
+document.querySelector('#elementoOndeVoceEsta').previousElementSibling;
+
+document.querySelector('#terceiroFilho').previousSibling;
+
+document.querySelector('#elementoOndeVoceEsta').nextElementSibling;
+
+document.querySelector('#pai').lastElementChild.previousElementSibling;
+
+
+
+
+
+
+
+// Acesse o elemento elementoOndeVoceEsta .
+// Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+// Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+// Acesse o primeiroFilho a partir de pai .
+// Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta .
+// Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta .
+// Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta .
+// Agora acesse o terceiroFilho a partir de pai .
