b7d8970d70fe2fd0ac9f03bdc175204d4b6f32b6
Leonidas Junior <leonautcco@gmail.com>
2021-07-06 12:16:57 -0300

    Solucao fixacao3

---
+++ exercises/bloco_5/dia_2/fixacao3.js
@@ -0,0 +1,19 @@
+// Remova todos os elementos filhos de paiDoPai exceto pai , elementoOndeVoceEsta e primeiroFilhoDoFilho .
+
+let segundoEUltimoFilhoDoFilho = document.getElementById("segundoEUltimoFilhoDoFilho");
+segundoEUltimoFilhoDoFilho.remove();
+
+let terceiroFilho = document.getElementById("terceiroFilho");
+terceiroFilho.remove();
+
+let quartoEUltimoFilho = document.getElementById("quartoEUltimoFilho");
+quartoEUltimoFilho.remove();
+
+let primeiroFilhoDoFilho = document.getElementById("primeiroFilhoDoFilho");
+primeiroFilhoDoFilho.remove();
+
+
+
+
+
+
