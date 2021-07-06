9adda4c1a5bafd36f6af6c906f24108f9304138c
Leonidas Junior <leonautcco@gmail.com>
2021-07-06 11:59:56 -0300

    Solucao fixacao2 e correcao fixacao1

--- exercises/bloco_5/dia_2/fixacao2.js
+++ exercises/bloco_5/dia_2/fixacao2.js
@@ -1,10 +1,23 @@
-
-
-
-
-
-
 // Crie um irmão para elementoOndeVoceEsta .
+let elemento_pai = document.querySelector('#elementoOndeVoceEsta').parentElement;
+let irmaoElemento = document.createElement('div');
+irmaoElemento.id = 'irmaoElementoOndeVoceEsta';
+elemento_pai.appendChild(irmaoElemento);
+
 // Crie um filho para elementoOndeVoceEsta .
+let paiOndeVoceEsta = document.querySelector('#elementoOndeVoceEsta');
+let filhoElemento = document.createElement ('p');
+filhoElemento.id = 'filhoElemento';
+filhoElemento.textContent = 'Este é o filho do elemento onde você está';
+paiOndeVoceEsta.appendChild(filhoElemento);
+
+
 // Crie um filho para primeiroFilhoDoFilho .
+let paiFilhoDoFilho = document.querySelector('#primeiroFilhoDoFilho');
+let filhoDoFilhoDoFilho = document.createElement ('p');
+filhoDoFilhoDoFilho.id = 'filhoDoFilhoDoFilho';
+filhoDoFilhoDoFilho.textContent = 'Este é o filho do primeiro filho do filho';
+paiFilhoDoFilho.appendChild(filhoDoFilhoDoFilho);
+
 // A partir desse filho criado, acesse terceiroFilho .
+document.querySelector('#filhoDoFilhoDoFilho').parentElement.parentElement.nextElementSibling;
