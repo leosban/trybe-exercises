0a171445fbc80a50766b77d7bdc11aac8b401a6f
Leonidas Junior <leonautcco@gmail.com>
2021-07-07 15:13:22 -0300

    solucao fixacao1

---
+++ exercises/bloco_5/dia_4/fixacao1.js
@@ -0,0 +1,12 @@
+document.cookie = 'email=leosban@icloud.com';
+
+document.cookie = 'email=leosban@icloud.com; expires=Fri, 16 Jul 2021 12:00:00 UTC';
+
+document.cookie = 'email=leosban@icloud.com; expires=Fri, 16 Jul 2021 12:00:00 UTC; path=/';
+
+let myCookie = document.cookie;
+console.log(myCookie);
+
+document.cookie = 'email=leosban@terra.com.br; expires=Fri, 16 Jul 2021 12:00:00 UTC';
+
+document.cookie = 'email=; expires=Mon, 05 Jul 2021 12:00:00 UTC;'
